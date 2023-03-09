7a35d8155ff5ef8a6929efc7a6cd2913d4e5d0cd
Graimi <113901525+Graimi@users.noreply.github.com>
2023-03-09 16:17:59 +0100

    trying to netlify

--- components/Shop/Shop.js
+++ components/Shop/Shop.js
@@ -1,11 +1,11 @@
 // import data from '../../data/Data.js';
-import filter from './Filter/Filter.js';
+// import filter from './Filter/Filter.js';
 // import products from './Products/Products.js';
 
 // Desde aquí lanzamos todas las funciones de la tienda
 const shop = () => {
   // products(data);
-  filter();
+  // filter();
 };
 
 export default shop;
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import './style.css';
 
 import functionInvisibleNav from './components/Header/Header.js';
-import shop from './components/Shop/Shop.js';
+// import shop from './components/Shop/Shop.js';
 import hero from './components/Hero/Hero.js';
 import whatsAppButton from './components/WhatsAppButton/WhatsApp.js';
 import footer from './components/Footer/Footer.js';
+import filter from './components/Shop/Filter/Filter';
 
 footer();
 
@@ -12,6 +13,7 @@ whatsAppButton();
 
 hero();
 
-shop();
+// shop();
+// filter();
 
-functionInvisibleNav();
+// functionInvisibleNav();
