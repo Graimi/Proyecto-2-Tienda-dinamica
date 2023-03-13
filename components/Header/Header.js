729880e97051ec4cb60eeb630b952290a358c9ee
Graimi <113901525+Graimi@users.noreply.github.com>
2023-03-13 20:45:55 +0100

    navbar adapted, everything done, time to try to upload to netlify

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -14,17 +14,22 @@ headerSection.innerHTML += headerTemplate({
 // A partir de aquí creamos el código para la invisible navbar
 const navBar = document.querySelector('.bc__section__header__layout');
 const heroSection = document.querySelector('#hero');
-
 const invisibleNav = () => {
   // No estoy seguro de por qué ha funcionado con .scrollY cuando debería ser con .scrollX, pura casualidad
-  // Se ha añadido el 90 para que la navbar aparezca antes de los productos y se vea mejor
-  if (window.scrollY * 1.45 > heroSection.offsetHeight) {
-    navBar.style.backgroundColor = '#191800';
+  // Se ha añadido el 80 para que la navbar aparezca tan pronto como se mueva la venta para no superponer los datos
+  if (window.scrollY * 80 > heroSection.offsetHeight) {
+    navBar.style.backgroundColor = 'var(--bc__color--black)';
   } else {
     navBar.style.backgroundColor = 'transparent';
   }
 };
-
 const functionInvisibleNav = window.addEventListener('scroll', invisibleNav);
+
+// Creamos la siguiente función para que no se vean en transparente las opciones cuando pinchemos en la hamburguesa
+const hamburger = document.querySelector('.bc__hamburguer__label');
+const hamburgerToggle = () => {
+  navBar.classList.toggle('toggle__hamburguer');
+};
+hamburger.addEventListener('click', hamburgerToggle);
 
 export default functionInvisibleNav;
